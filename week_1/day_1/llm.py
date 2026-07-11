ff98611e437915ce5ed6f7874356ad2c67118f83
Ayush Pal <Ayushpalworkplace2006@gmail.com>
2026-07-12 04:09:24 +0530

    Add initial files for day-1 and day-2, including Python scripts and project configuration

---
+++ week_1/day_1/llm.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("GROQ_API_KEY environment variable is not set.")
+
+Client = Groq(api_key=my_api_key)
+
+model = "llama-3.3-70b-versatile"
+role = "user"
+content = "Write a code for fibonacci series in python"
+message = [{"role": role, "content": content}]
+response = Client.chat.completions.create(
+    model=model,
+    messages=message,
+)
+print(response.choices[0].message.content)
