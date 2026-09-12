e2f4d04b9a4c8095121c2a635d34ab0fbf4db875
Ayush Pal <144852243+Ayushpal2006@users.noreply.github.com>
2026-09-12 05:41:04 +0530

    Good prompt technique

--- learnings/4.prompt_engg/prompt_engg.py
+++ learnings/4.prompt_engg/prompt_engg.py
@@ -21,3 +21,23 @@ def llm_ans(prompt):
     response=client.chat.completions.create(model=model, messages=messages)
     ans=response.choices[0].message.content
     return ans
+
+
+good_prompt="""
+#ROLE:
+You are a support assistant at a mobile/laptop company
+#TASK
+You have to classify the issue in a category
+#CONSTRAINT
+You have to classify the issue in one of three categories namely billing, technical, return.
+#OUTPUT FORMAT
+Your answer should be in one word only. The one word shoud be one of the categories given in constraints
+#Example
+For instance if a user compalin says he wants a refund then the category is Return
+#FALLBACK
+If the issue is unrelated to any of the categories mentioned in constraints, then the answer should be OTHER
+This is a user complaint:
+My marriage is broke
+"""
+
+print(llm_ans(good_prompt))
