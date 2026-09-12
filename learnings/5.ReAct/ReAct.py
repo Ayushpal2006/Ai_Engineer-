3e76a5178a7f8acff8193c38b70876f9498309ea
Ayush Pal <144852243+Ayushpal2006@users.noreply.github.com>
2026-09-12 05:55:30 +0530

    Add initial files for 5.ReAct project including Python scripts, configuration, and dependencies

---
+++ learnings/5.ReAct/ReAct.py
@@ -0,0 +1,6 @@
+import os
+from pathlib import Path
+from time import sleep
+from dotenv import load_dotenv
+from groq import Groq
+import re
---
+++ learnings/5.ReAct/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 5-react!")
+
+
+if __name__ == "__main__":
+    main()
