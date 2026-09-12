6be9e73b3502a10fd380c285a5ab3a21259cb668
Ayush Pal <144852243+Ayushpal2006@users.noreply.github.com>
2026-09-12 05:37:51 +0530

    Add dependencies for prompt engineering and implement initial prompt handling script

---
+++ learnings/4.prompt_engg/prompt_engg.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+model="openai/gpt-oss-120b"
+
+def llm_ans(prompt):
+    message={
+        "role":"user",
+        "content": prompt
+    }
+    messages=[message]
+    response=client.chat.completions.create(model=model, messages=messages)
+    ans=response.choices[0].message.content
+    return ans
